1ecf640c02e6228c25d108f1ce7d23f5a05ca095
JsT02 <117226386+JsT02@users.noreply.github.com>
2022-11-01 23:27:02 -0400

    Update producerconsumer.h

--- producerconsumer.h
+++ producerconsumer.h
@@ -2,7 +2,7 @@
 *  11/1/2022
 *  Producer Consumer Problem */
 
-*/ This file contains all global variables, the buffer, and the structure item in use
+/* This file contains all global variables, the buffer, and the structure item in use
 *  across all project files */
 
 #ifndef PRODUCERCONSUMER_H
